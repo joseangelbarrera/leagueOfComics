eecbf6ae58724fbfa91fe5d95f218a329ea5b503
Yair Silva Mendoza <yairs.mendoza@hotmail.com>
2017-05-04 10:39:59 +0200

    add header and footer

--- js/config.js
+++ js/config.js
@@ -1,18 +1,18 @@
 angular.module('comicApp')
-    .config(function ($routeProvider) {
-      $routeProvider
-            .when('/', {
-              templateUrl: 'templates/comic_search.html',
-              controller: 'searchController'
-            })
+    .config(function($routeProvider) {
+        $routeProvider
+        .when('/', {
+                templateUrl: 'templates/comic_search.html',
+                controller: 'searchController'
+        })
 
-                 .when('/comic_results/:searchResults', {
-                   templateUrl: 'templates/comic_results.html',
-                   controller: 'resultsController'
-                 })
+        .when('/comic_results/:searchResults', {
+            templateUrl: 'templates/comic_results.html',
+            controller: 'resultsController'
+        })
 
-            .when('/comic_info/:infoResults', {
-              templateUrl: '/templates/comic_info.html',
-              controller: 'infoController'
-            })
+        .when('/comic_info/:infoResults', {
+            templateUrl: '/templates/comic_info.html',
+            controller: 'infoController'
+        })
     })
--- js/controllers/comic_info_controller.js
+++ js/controllers/comic_info_controller.js
@@ -1,26 +1,7 @@
 angular.module('comicApp')
 
-  .controller('infoController', function ($scope, comicAppService) {   // Function with '$scope'(from html) and 'gameAppService' from service.js
-    $scope.comicInput = ''     // That recives the input value from ng-model(index.html)
-    $scope.searchComicButton = function () {
-      console.log($scope.comicInput)
-
-      comicAppService
-               .searchComics($scope.comicInput)
-               .then(function (response) {
-                 $scope.comiclist = response.data.results
-                 console.log($scope.comiclist)
-               })
-    }
-  })
-
  .controller('infoController', function ($scope, $rootScope, $routeParams, comicAppService) { // Function with '$scope'(from html) and 'gameAppService' from service.js
+$rootScope.section = 'comic_results'
   
-   console.log($routeParams)
-   comicAppService.searchComics($routeParams.searchResults)
-             .then(function (response) {
-               $scope.comiclist = response.data.results
-               console.log($scope.comiclist)
-             })
+   }
  })
-
